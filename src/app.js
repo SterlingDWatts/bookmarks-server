4a3802ab8129cf29e8dafbf80758e4ddf69de307
Sterling Watts <SterlingDWatts@gmail.com>
2020-01-30 15:11:48 -0800

    Create logger and authentication middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
+const winston = require("winston");
 const { NODE_ENV } = require("./config");
 
 // create Express app
@@ -16,10 +17,62 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
+// logger middleware
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "info.log" })]
+});
+
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
+
+// authentication middleware
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`);
+    return res.status(401).json({ error: "Unauthorized request." });
+  }
+
+  next();
+});
+
 // basic endpoint for app.js
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+// use to parse Json from the body
+const bodyParser = express.json();
+
+// /bookmarks endpoint
+const bookmarksRouter = express.Router();
+
+bookmarksRouter
+  .route("/bookmarks")
+  .get((req, res) => {
+    // TODO implement code
+  })
+  .post(bodyParser, (req, res) => {
+    // TODO implement code
+  });
+
+bookmarksRouter
+  .route("/bookmarks/:id")
+  .get((req, res) => {
+    // TODO implement code here
+  })
+  .delete((req, res) => {
+    // TODO implement code here
+  });
 
 // error handling middleware gives short response if in production
 app.use(function errorHandler(error, req, res, next) {
