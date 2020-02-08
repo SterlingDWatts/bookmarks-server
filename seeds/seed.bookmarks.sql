517ad4a02cc62885ae7137af31256c858b866c91
Sterling Watts <SterlingDWatts@gmail.com>
2020-02-08 15:53:32 -0800

    Add seed for bookmarks table

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+  ('Google', 'https://www.google.com', 5, 'Search the web!'),
+  ('Thinkful', 'https://www.thinkful.com', 5, 'Think outside the classroom'),
+  ('DataQuest', 'https://www.dataquest.io', 4, 'Learn Data Science'),
+  ('Bloc', 'https://www.bloc.io', 5, 'Learn Software Development'),
+  ('MDN', 'https://developer.mozilla.org', 5, 'The only place to find web documentation');
